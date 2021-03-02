d5dbaed84369cc00ab0a6b5812114382fee39661
jjleong93 <jj.leong@techninier.com>
2021-03-02 21:21:22 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { render } from "react-dom";
+//import Pet from "./Pet";
+import { Link, Router } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const themeHook = useState("darkblue");
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Luna",
+  //     animal: "Dog",
+  //     breed: "Havanese",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Cockatiel",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Doink",
+  //     animal: "Cat",
+  //     breed: "Stray",
+  //   }),
+  // ]);
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
---
+++ src/useDropdown.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const useDropdown = (label, defaultState, options) => {
+  const [state, updateState] = useState(defaultState);
+  const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
+  const Dropdown = () => {
+    return (
+      <label htmlFor={id}>
+        {label}
+        <select
+          id={id}
+          value={state}
+          onChange={(e) => updateState(e.target.value)}
+          onBlur={(e) => updateState(e.target.value)}
+          disabled={options.length === 0}
+        >
+          {options.map((item) => {
+            return (
+              <option key={item} value={item}>
+                {item}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+    );
+  };
+  return [state, Dropdown, updateState];
+};
+
+export default useDropdown;
