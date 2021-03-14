8b4680890f67b594863956a6bee9cdbb9e5c692d
jjleong93 <jj.leong@techninier.com>
2021-03-14 23:47:12 +0800

    - Completed SSR

--- src/App.js
+++ src/App.js
@@ -7,41 +7,42 @@ import Details from "./Details";
 import ThemeContext from "./ThemeContext";
 
 const App = () => {
-  const themeHook = useState("darkblue");
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Luna",
-  //     animal: "Dog",
-  //     breed: "Havanese",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Pepper",
-  //     animal: "Bird",
-  //     breed: "Cockatiel",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Doink",
-  //     animal: "Cat",
-  //     breed: "Stray",
-  //   }),
-  // ]);
-  return (
-    <React.StrictMode>
-      <ThemeContext.Provider value={themeHook}>
-        <div>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
+    const themeHook = useState("darkblue");
+    // return React.createElement("div", {}, [
+    //   React.createElement("h1", {}, "Adopt Me!"),
+    //   React.createElement(Pet, {
+    //     name: "Luna",
+    //     animal: "Dog",
+    //     breed: "Havanese",
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Pepper",
+    //     animal: "Bird",
+    //     breed: "Cockatiel",
+    //   }),
+    //   React.createElement(Pet, {
+    //     name: "Doink",
+    //     animal: "Cat",
+    //     breed: "Stray",
+    //   }),
+    // ]);
+    return (
+        <React.StrictMode>
+            <ThemeContext.Provider value={themeHook}>
+                <div>
+                    <header>
+                        <Link to="/">Adopt Me!</Link>
+                    </header>
 
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
-        </div>
-      </ThemeContext.Provider>
-    </React.StrictMode>
-  );
+                    <Router>
+                        <SearchParams path="/" />
+                        <Details path="/details/:id" />
+                    </Router>
+                </div>
+            </ThemeContext.Provider>
+        </React.StrictMode>
+    );
 };
 
-render(<App />, document.getElementById("root"));
+export default App;
+//render(<App />, document.getElementById("root"));
